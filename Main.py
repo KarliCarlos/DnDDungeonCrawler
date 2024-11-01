62253956a41cdb735e0c06564cf2185b163f9226
KarliCarlos <88288166+KarliCarlos@users.noreply.github.com>
2024-11-01 21:21:18 +0100

    Added Movement between Tiles, removed Tile.py bc it was unnecessary

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pygame as pg
-from Tile import Tile
 import os
+
+import pprint
 
 pg.init()
 
@@ -11,13 +12,12 @@ class Main:
 
         self.EDITMODE = True
         self.SIZE = 1024
+        self.MAPSIZE = 10
 
 ####### VARIABLES #######
 
         self.screen = pg.display.set_mode((self.SIZE, self.SIZE))
         self.clock = pg.time.Clock()
-
-        self.currentTile = None
 
         self.loadedTiles = {
             "OneWay": [],
@@ -33,22 +33,22 @@ class Main:
         self.currentType = [0, 0]
         self.types = [k for k in self.loadedTiles.keys()]
 
+        self.map = [[None for _ in range(self.MAPSIZE)] for _ in range(self.MAPSIZE)]
+        self.coords = [int(self.MAPSIZE/2), int(self.MAPSIZE/2)]
 
 ### FUNCTIONS ###
 
-    def createTile(self):
-        self.currentType = [0, 0]
-        self.currentTile = Tile(self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]])
-
     def reloadTileImg(self):
-        if not self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]]:
-            self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]] = pg.transform.scale(pg.image.load(os.path.join('Tiles', self.types[self.currentType[0]], str(self.currentType[1] + 1) + '.jpg')), (1024, 1024))
-        self.currentTile.img = self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]]
+        if not self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]]: #Wurde das Bild schonmal geladen?
+            self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]] = pg.transform.scale(pg.image.load(os.path.join('Tiles', self.types[self.currentType[0]], str(self.currentType[1] + 1) + '.jpg')), (1024, 1024)) #Bild wird geladen
+        self.map[self.coords[1]][self.coords[0]] = (self.types[self.currentType[0]], self.currentType[1])
 
     def drawCurrentTile(self):
-        if not self.currentTile.img:
+        if not self.map[self.coords[1]][self.coords[0]]: #Wenn noch kein Tile vorhanden -> Neues Tile
+            self.currentType = [0, 0]
             self.reloadTileImg()
-        self.screen.blit(self.currentTile.img, (0,0))
+        imgPointer = self.map[self.coords[1]][self.coords[0]]
+        self.screen.blit(self.loadedTiles[imgPointer[0]][imgPointer[1]], (0,0)) #Bild wird angezeigt
 
 ### GAME LOOP ###
 
@@ -60,6 +60,7 @@ class Main:
                     exit()
                 if self.EDITMODE == True:
                     if e.type == pg.KEYUP:
+                        ## Change Imgs ##
                         if e.key == pg.K_w:
                             if self.currentType[0] == 4:
                                 self.currentType[0] = 0
@@ -90,10 +91,26 @@ class Main:
                                 self.currentType[1] += 1
                             self.reloadTileImg()
 
-            if self.currentTile == None:
-                self.createTile()
+                        ## Change Tiles ##
 
-            self.drawCurrentTile()            
+                        if e.key == pg.K_UP:
+                            if self.coords[1] > 0:
+                                self.coords[1] -= 1
+
+                        if e.key == pg.K_DOWN:
+                            if self.coords[1] < self.MAPSIZE - 1:
+                                self.coords[1] += 1
+
+                        if e.key == pg.K_LEFT:
+                            if self.coords[0] > 0:
+                                self.coords[0] -= 1
+
+                        if e.key == pg.K_RIGHT:
+                            if self.coords[0] < self.MAPSIZE - 1:
+                                self.coords[0] += 1
+
+            self.drawCurrentTile()        
+            pprint.pprint(self.map)
 
             pg.display.flip()
             self.clock.tick(30)
