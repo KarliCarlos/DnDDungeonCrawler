12c3fb9392630b2a76e3424ab81afc59f4446e46
KarliCarlos <88288166+KarliCarlos@users.noreply.github.com>
2024-11-02 17:52:33 +0100

    Added possibility to save and load and redesigned cache

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import os
-
-import pprint
+from tkinter import filedialog
+import json
 
 pg.init()
 
@@ -19,19 +19,10 @@ class Main:
         self.screen = pg.display.set_mode((self.SIZE, self.SIZE))
         self.clock = pg.time.Clock()
 
-        self.loadedTiles = {
-            "OneWay": [],
-            "TwoWay": [],
-            "ThreeWay": [],
-            "FourWay": [],
-            "HallWay": []
-        }
-        for k,v in self.loadedTiles.items():
-            for i in os.listdir(os.path.join('Tiles', k)):
-                v.append(None)
+        self.types = [t for t in os.listdir(os.path.join('Tiles'))]
+        self.loadedTiles = [[None for _ in os.listdir(os.path.join('Tiles', k))] for k in self.types]
 
         self.currentType = [0, 0]
-        self.types = [k for k in self.loadedTiles.keys()]
 
         self.map = [[None for _ in range(self.MAPSIZE)] for _ in range(self.MAPSIZE)]
         self.coords = [int(self.MAPSIZE/2), int(self.MAPSIZE/2)]
@@ -39,9 +30,9 @@ class Main:
 ### FUNCTIONS ###
 
     def reloadTileImg(self):
-        if not self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]]: #Wurde das Bild schonmal geladen?
-            self.loadedTiles[self.types[self.currentType[0]]][self.currentType[1]] = pg.transform.scale(pg.image.load(os.path.join('Tiles', self.types[self.currentType[0]], str(self.currentType[1] + 1) + '.jpg')), (1024, 1024)) #Bild wird geladen
-        self.map[self.coords[1]][self.coords[0]] = (self.types[self.currentType[0]], self.currentType[1])
+        if not self.loadedTiles[self.currentType[0]][self.currentType[1]]: #Wurde das Bild schonmal geladen?
+            self.loadedTiles[self.currentType[0]][self.currentType[1]] = pg.transform.scale(pg.image.load(os.path.join('Tiles', self.types[self.currentType[0]], str(self.currentType[1] + 1) + '.jpg')), (1024, 1024)) #Bild wird geladen
+        self.map[self.coords[1]][self.coords[0]] = (self.currentType[0], self.currentType[1])
 
     def drawCurrentTile(self):
         if not self.map[self.coords[1]][self.coords[0]]: #Wenn noch kein Tile vorhanden -> Neues Tile
@@ -49,6 +40,28 @@ class Main:
             self.reloadTileImg()
         imgPointer = self.map[self.coords[1]][self.coords[0]]
         self.screen.blit(self.loadedTiles[imgPointer[0]][imgPointer[1]], (0,0)) #Bild wird angezeigt
+
+    def save(self):
+        file = filedialog.asksaveasfile(mode='w', defaultextension='.json')
+        if not file:
+            return
+        json.dump(self.map, file)
+        file.close()
+
+    def load(self):
+        file = filedialog.askopenfile(defaultextension='.json')
+        if not file:
+            return
+        self.map = json.load(file)
+        file.close()
+
+        for i in self.map:
+            for j in i:
+                if j:
+                    self.currentType = j
+                    if not self.loadedTiles[self.currentType[0]][self.currentType[1]]:
+                        self.loadedTiles[self.currentType[0]][self.currentType[1]] = pg.transform.scale(pg.image.load(os.path.join('Tiles', self.types[self.currentType[0]], str(self.currentType[1] + 1) + '.jpg')), (1024, 1024)) #Bild wird geladen
+
 
 ### GAME LOOP ###
 
@@ -79,13 +92,13 @@ class Main:
 
                         if e.key == pg.K_a:
                             if self.currentType[1] == 0:
-                                self.currentType[1] = len(self.loadedTiles[self.types[self.currentType[0]]]) - 1
+                                self.currentType[1] = len(self.loadedTiles[self.currentType[0]]) - 1
                             else:
                                 self.currentType[1] -= 1
                             self.reloadTileImg()
 
                         if e.key == pg.K_d:
-                            if self.currentType[1] == len(self.loadedTiles[self.types[self.currentType[0]]]) - 1:
+                            if self.currentType[1] == len(self.loadedTiles[self.currentType[0]]) - 1:
                                 self.currentType[1] = 0
                             else:
                                 self.currentType[1] += 1
@@ -109,8 +122,13 @@ class Main:
                             if self.coords[0] < self.MAPSIZE - 1:
                                 self.coords[0] += 1
 
+                        if e.key == pg.K_RETURN:
+                            self.save()
+
+                        if e.key == pg.K_l:
+                            self.load()
+
             self.drawCurrentTile()        
-            pprint.pprint(self.map)
 
             pg.display.flip()
             self.clock.tick(30)
